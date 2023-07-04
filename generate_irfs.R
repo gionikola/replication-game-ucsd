864fb70448a7568cfb7b9c410412a4a6b6ef5384
Giorgi Nikolaishvili <gnikolai@uoregon.edu>
2023-07-04 08:47:11 -0700

    Remove unnecessary IRFs

--- generate_irfs.R
+++ generate_irfs.R
@@ -19,6 +19,7 @@ irf <- function(A_mats, impact_mat, gamma_opt, h){
 
 ###################################################
 ### Generate IRFs
+h=30
 irfs <- irf(A_mats, impact_mat, -gamma_opt, h) * 100
 
 irfs_df <- data.frame(horizon = c(0:h),
@@ -36,29 +37,6 @@ irfs_df_long$variable <- factor(irfs_df_long$variable, levels = c("tfp","output"
 
 ####################################################
 ### Plot IRFs
-ggplot(irfs_df_long %>% filter(variable == names(irfs_df)[2]), aes(x=horizon, y=response, linetype=variable)) +
-  geom_line() +
-  theme_bw()
-ggplot(irfs_df_long %>% filter(variable == names(irfs_df)[3]), aes(x=horizon, y=response, linetype=variable)) +
-  geom_line() +
-  theme_bw()
-ggplot(irfs_df_long %>% filter(variable == names(irfs_df)[4]), aes(x=horizon, y=response, linetype=variable)) +
-  geom_line() +
-  theme_bw()
-ggplot(irfs_df_long %>% filter(variable == names(irfs_df)[5]), aes(x=horizon, y=response, linetype=variable)) +
-  geom_line() +
-  theme_bw()
-ggplot(irfs_df_long %>% filter(variable == names(irfs_df)[6]), aes(x=horizon, y=response, linetype=variable)) +
-  geom_line() +
-  theme_bw()
-ggplot(irfs_df_long %>% filter(variable == names(irfs_df)[7]), aes(x=horizon, y=response, linetype=variable)) +
-  geom_line() +
-  theme_bw()
-ggplot(irfs_df_long %>% filter(variable == names(irfs_df)[8]), aes(x=horizon, y=response, linetype=variable)) +
-  geom_line() +
-  theme_bw()
-
-
 ggplot(irfs_df_long, aes(x=horizon,y=response)) +
   geom_line() +
   facet_wrap(. ~ variable, scale = "free", nrow=2) +
